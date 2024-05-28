c778657095aa6a8c4e35f9e127abed0a2cc50c57
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-05-28 17:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById("modal");
+    var openModalBtn = document.getElementById("openModal");
+    var closeModalBtn = document.querySelector(".close-modal");
+
+    // Open modal
+    openModalBtn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // Close modal when close button is clicked
+    closeModalBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Close modal when clicked outside of the modal content
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
